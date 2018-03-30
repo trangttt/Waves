b1e6531841154a8f48150c5cdbaa0b808328fbd5
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-03-30 14:28:10 +0300

    NODE-601 CryptoBenchmark uses Global

--- lang/jvm/src/main/scala/com/wavesplatform/lang/benchmark/CryptoBenchmark.scala
+++ lang/jvm/src/main/scala/com/wavesplatform/lang/benchmark/CryptoBenchmark.scala
@@ -2,20 +2,21 @@ package com.wavesplatform.lang.benchmark
 
 import java.util.concurrent.ThreadLocalRandom
 
+import com.wavesplatform.lang.Global
 import com.wavesplatform.lang.benchmark.CryptoBenchmark._
-import com.wavesplatform.lang.jvm.Crypto
 import org.openjdk.jmh.annotations.Benchmark
 import scorex.crypto.signatures.{Curve25519, PrivateKey, PublicKey, Signature}
 
-class CryptoBenchmark extends Crypto {
-  @Benchmark
-  def sha256_test(): Array[Byte] = sha256(randomBytes(DataBytesLength))
+class CryptoBenchmark {
 
   @Benchmark
-  def keccack256_test(): Array[Byte] = keccack256(randomBytes(DataBytesLength))
+  def sha256_test(): Array[Byte] = Global.sha256(randomBytes(DataBytesLength))
 
   @Benchmark
-  def blake2b256_test(): Array[Byte] = blake2b256(randomBytes(DataBytesLength))
+  def keccack256_test(): Array[Byte] = Global.keccack256(randomBytes(DataBytesLength))
+
+  @Benchmark
+  def blake2b256_test(): Array[Byte] = Global.blake2b256(randomBytes(DataBytesLength))
 
   @Benchmark
   def curve25519_generateKeypair_test(): (PrivateKey, PublicKey) = curve25519.generateKeypair
@@ -29,8 +30,8 @@ class CryptoBenchmark extends Crypto {
   @Benchmark
   def curve25519_full_test(): Boolean = {
     val (privateKey, publicKey) = curve25519.generateKeypair
-    val message = randomBytes(DataBytesLength)
-    val signature = curve25519.sign(privateKey, message)
+    val message                 = randomBytes(DataBytesLength)
+    val signature               = curve25519.sign(privateKey, message)
     Curve25519.verify(Signature @@ signature, message, publicKey)
   }
 }
@@ -46,7 +47,7 @@ object CryptoBenchmark {
   }
 
   object curve25519 {
-    def generateKeypair: (PrivateKey, PublicKey) = Curve25519.createKeyPair(randomBytes(SeedBytesLength))
+    def generateKeypair: (PrivateKey, PublicKey)                        = Curve25519.createKeyPair(randomBytes(SeedBytesLength))
     def sign(privateKey: PrivateKey, message: Array[Byte]): Array[Byte] = Curve25519.sign(privateKey, message)
   }
 }
